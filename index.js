8441cb3dc77dc87334d49868500d195eb6743a86
Omar Richards <omarrichards01@gmail.com>
2023-08-31 00:34:40 -0400

    lab

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // Write your code here!
-const newHeader = document.createElement("h1")
 
-newHeader.id = 'victory'
-newHeader.innerHTML = "Omar Richards is the champion"
+
+
+const newHeader = document.createElement("h1")
+newHeader.id = "victory"
+newHeader.textContent = "Omar is the champion"
